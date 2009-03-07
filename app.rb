26f22cac8a219a9ab905302c8d771c0b6b48d0e3
Graham Ashton <graham@effectif.com>
2009-03-07 14:48:10 +0000

    First version that can actually do stuff.

---
+++ app.rb
@@ -0,0 +1,35 @@
+require "rubygems"
+require "sinatra"
+
+class TwfyFinder
+  def self.execute(command)
+    # This method exists so we can stub it out in the specs to avoid running
+    # kwexplorer during testing.
+    json = `#{command}`
+    raise RuntimeError.new(json.split("\n").first) unless $?.exitstatus == 0
+    json
+  end
+  
+  def initialize(options)
+    @options = options
+  end
+  
+  def command(query)
+    "kwexplorer --json --twfy-minister '#{query}' --remove-subphrases #{@options}"
+  end
+  
+  def find(query)
+    TwfyFinder.execute(command(query))
+  end
+end
+
+before { request.env['PATH_INFO'].gsub!(/\/$/, '') }
+
+get "" do
+  haml :index
+end
+
+get "/minister" do
+  query = TwfyFinder.new("--min-words 2 --max-words 3")
+  query.find(params[:q])
+end
